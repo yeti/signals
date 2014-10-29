985760bd632c8c9c91b034bd60a90d2def8be0ca
Collin Canard Schupman <collinschupman@gmail.com>
2014-10-28 17:18:29 -0700

    new scripts

--- manticom_ios_coredata.py
+++ manticom_ios_coredata.py
@@ -12,17 +12,16 @@
 # this script will make appropiate changes and update the file
 
 # TODO:
-#
-# add in interactive session to get JSON and XML to update
-# handle O2O case
-# handle O2N case?
-# test array of primitive case
+# O2O case
 # correctly resize elements, dynamically
 
 import StringIO
 from lxml import etree
 from xml.dom import minidom
 import shutil
+import json
+import manticom_ios_datamodels
+import inflect
 
 # Conversions for use in CoreData
 DATA_TYPES = {
@@ -46,23 +45,25 @@ DATA_TYPES = {
 RELATIONSHIPS = set({
     "O2O",
     "M2M",
-    "O2M"
+    "O2M",
+    "M2O"
 })
 
 # A little logic to to make plural words more readable
 # this won't cover everything, there's a few python libraries out there you could use: https://pypi.python.org/pypi/inflect
 def get_word_plural(word):
+    p = inflect.engine()
     if word.endswith("y"):
-        return word.lower()[:-1] + 'ies'
+        return word[:-1] + 'ies'
     else:
-        return word.lower() + 's'
+        return word + 's'
 
 # Adds an elements branch and element leaf for each object in the JSON file
 # TODO: come up with better way to compute height/position
 def add_elements(model, objects):
     elements = etree.SubElement(model, "elements")
     for obj in objects:
-        element = etree.SubElement(elements, "element", name = obj.keys()[0][1:])
+        element = etree.SubElement(elements, "element", name = obj.keys()[0][1].upper() + obj.keys()[0][2:])
         element.set("positionX", "0")
         element.set("positionY", "0")
         element.set("width", "128")
@@ -92,29 +93,30 @@ def add_many_relationship(entity_one, entity_two, entity_one_name, entity_two_na
     relationship.set("ordered", "YES")
     relationship.set("toMany", "YES")
 
-# Adds O2M relationships between these two entities
-def add_M2M_relationships(entity_one, entity_two):
-    entity_one_name = get_word_plural(entity_one.get('name'))
-    entity_two_name = get_word_plural(entity_two.get('name'))
-
-    add_many_relationship(entity_one, entity_two, entity_one_name, entity_two_name)
-    add_many_relationship(entity_two, entity_one, entity_two_name, entity_one_name)
+# Doing stuff
+def add_M2O_relationship(many_entity, one_entity, many_entities_relationship_name, one_entities_relationship_name):
+    add_many_relationship(one_entity, many_entity, one_entities_relationship_name, many_entities_relationship_name)
+    add_one_relationship(many_entity, one_entity, many_entities_relationship_name, one_entities_relationship_name)
 
 # Adds O2M relationships between these two entities
-def add_O2M_relationships(one_entity, many_entity):
-    one_entity_name = one_entity.get('name').lower()
-    many_entity_name = get_word_plural(many_entity.get('name'))
-    add_many_relationship(one_entity, many_entity, one_entity_name, many_entity_name)
-    add_one_relationship(many_entity, one_entity, many_entity_name, one_entity_name)
+def add_M2M_relationships(entity_one, entity_two, entity_one_relationship_name, entity_two_relationship_name):
+    add_many_relationship(entity_one, entity_two, entity_two_relationship_name, entity_one_relationship_name)
+    add_many_relationship(entity_two, entity_one, entity_one_relationship_name, entity_two_relationship_name)
+
+# Adds O2M relationships between these two entities
+
+def add_O2M_relationships(many_entity, one_entity, many_entities_relationship_name, one_entities_relationship_name):  
+    add_many_relationship(many_entity, one_entity, many_entities_relationship_name, one_entities_relationship_name)
+    add_one_relationship(one_entity, many_entity, one_entities_relationship_name, many_entities_relationship_name)
 
 # Adds relationships and inverse relationships for each required entity
-# Currently only supports M2M, O2M. TODO: Add in others
+# Currently only supports M2M, O2M. TODO: dd in others
 def add_relationships(model, objects):
     for obj in objects:
         obj_name = obj.keys()[0]
         obj_fields = obj[obj_name]
-        for key, value in obj_fields.iteritems():
 
+        for key, value in obj_fields.iteritems():
             values = value.split(",")
             if "optional" in values:
                 values.remove("optional")
@@ -127,48 +129,74 @@ def add_relationships(model, objects):
             if values[0] == "O2M":
                 one_entity = None
                 many_entity = None
+
                 for entity in model.iter("entity"):
-                    if entity.get('name') == obj_name[1:]:
-                        many_entity = entity
-                    elif entity.get('name') == values[1][1:]:
+                    if entity.get('name') == obj_name[1].upper() + obj_name[2:]:
                         one_entity = entity
-                add_O2M_relationships(one_entity, many_entity)
+                    elif entity.get('name') == values[1][1].upper() + values[1][2:]:
+                        many_entity = entity 
+                add_O2M_relationships(one_entity, many_entity, one_entity.get('name').lower().replace("response",""), key)
+
             elif values[0] == "M2M":
                 entity_one = None
                 entity_two = None
                 for entity in model.iter("entity"):
-                    if entity.get('name') == obj_name[1:]:
+                    if entity.get('name') == obj_name[1].upper() + obj_name[2:]:
                         entity_one = entity
-                    elif entity.get('name') == values[1][1:]:
+                    elif entity.get('name') == values[1][1].upper() + values[1][2:]:
                         entity_two = entity
-                add_M2M_relationships(entity_one, entity_two)
+                add_M2M_relationships(entity_one, entity_two, key, get_word_plural(entity_one.get('name').lower().replace("response","")))
+
+            elif values[0] == "M2O":
+                many_entity = None
+                one_entity  = None
+                for entity in model.iter("entity"):
+                    if entity.get('name') == obj_name[1].upper() + obj_name[2:]:
+                        many_entity = entity
+                    elif entity.get('name') == values[1][1].upper() + values[1][2:]:
+                        one_entity = entity
+                add_M2O_relationship(many_entity, one_entity, get_word_plural(many_entity.get('name').lower().replace("response","")), key)
+
     
 # Adds attributes to the given entity for its name and type
-def add_entity_attribute(entity, name, att_type):
+def add_entity_attribute(entity, name, att_type, optional):
+    if name == "id":
+        name = "theID"
+    elif name == "description":
+        name = "theDescription"
+    elif '_' in name:
+        words = name.split('_')
+        name = words[0]
+        for x in range(1,len(words)):
+
+            next_word = words[x]
+            next_word = next_word.capitalize()
+            name += next_word
+
     attribute = etree.SubElement(entity, "attribute", name = name)
-    attribute.set("optional", "YES")
+    if optional:
+        attribute.set("optional", "YES")
+    else:
+        attribute.set("optional", "YES")
     attribute.set("attributeType", att_type)
     attribute.set("syncable", "YES")
 
 # Adds entities for each object in the JSON tree
 def add_entities(model, objects):
     for obj in objects:
-        obj_name = obj.keys()[0]
-        entity = etree.SubElement(model, "entity", name = obj_name[1:])
-        obj_fields = obj[obj_name]
+        if "Parameters" not in obj.keys()[0]:
+            obj_name = obj.keys()[0]
+            entity = etree.SubElement(model, "entity", name = obj_name[1].upper() + obj_name[2:])
+            obj_fields = obj[obj_name]
+            for key, value in obj_fields.iteritems():
+                values = value.split(",")
+                if "primarykey" in values:
+                    values.remove("primarykey")
 
-        for key, value in obj_fields.iteritems():
-            values = value.split(",")
-
-            if "optional" in values:
-                values.remove("optional")
-            if "primarykey" in values:
-                values.remove("primarykey")
-
-            if values[0] == "array":
-                add_entity_attribute(entity, key, DATA_TYPES[values[1]])
-            elif values[0] not in RELATIONSHIPS:
-                add_entity_attribute(entity, key, DATA_TYPES[values[0]])
+                if values[0] == "array":
+                    add_entity_attribute(entity, key, DATA_TYPES[values[1]], "optional" in values)
+                elif values[0] not in RELATIONSHIPS and values[0]:
+                    add_entity_attribute(entity, key, DATA_TYPES[values[0]], "optional" in values)
 
 # Parses the given XML's model element and creates our initial root 
 def get_model(xml):
@@ -187,6 +215,7 @@ def write_xml_to_file(xml, objects):
     print "Adding entitites"
     add_entities(model,objects)
 
+
     print "Adding relationships"
     add_relationships(model, objects)
 
@@ -201,3 +230,19 @@ def write_xml_to_file(xml, objects):
     print "Writting XML"
     tree = etree.ElementTree(model)
     tree.write(xml, pretty_print=True)
+    #tree.write("new.xml", pretty_print=True)
+
+if __name__ == "__main__":
+    print "Loading XML and JSON"
+    incoming_json = "/Users/Collin/Documents/Yeti/viddit/manticom-schema-1.0.json"
+    xml = "/Users/Collin/Documents/Yeti/viddit-ios/Viddit/Common/Models/VidditModel.xcdatamodeld/VidditModel.xcdatamodel/contents"
+    with open(incoming_json, "r") as f:
+        print "Reading SML"
+        read_json = json.loads(f.read())
+        objects = read_json["objects"]
+        urls = read_json["urls"] 
+        print "Writting XML"
+        #write_xml_to_file(xml,objects)
+
+        print "Writting URLS"
+        manticom_ios_datamodels.create_mappings(urls, objects)
