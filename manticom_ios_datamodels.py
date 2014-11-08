4f36733b933e13b9cd52b0370eec8364f749eade
Collin Canard Schupman <collinschupman@gmail.com>
2014-11-07 17:48:33 -0800

    latest updates

--- manticom_ios_datamodels.py
+++ manticom_ios_datamodels.py
@@ -4,6 +4,11 @@ from xml.dom import minidom
 import shutil
 import json
 
+# Add map to get Entities
+# Create dynamic objects in the correct way
+# need to add in content type
+
+
 OBJC_DATA_TYPES = {
     "date"       : "NSDate*",
     "datetime"   : "NSDate*",
@@ -29,8 +34,32 @@ params = {}
 
 requests = {}
 
-def create_relationships_and_properties(urls):
-    pass
+def write_login_to_files(h_file, m_file):
+    h_file.write('\n')
+    h_file.write('- (void)getAllLoginWithUsername:(NSString*)username password:(NSString*)password success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure {\n')
+    h_file.write('   RKObjectManager* sharedMgr = [RKObjectManager sharedManager];\n')
+    h_file.write('   [sharedMgr.HTTPClient setAuthorizationHeaderWithUsername:username password:password];\n')
+    h_file.write('   [sharedMgr getObjectsAtPath:@"login/" parameters:nil success:success failure:failure];\n')
+    h_file.write('}\n')
+    m_file.write('- (void)getAllLoginWithUsername:(NSString*)username password:(NSString*)password success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n')
+
+def function():
+        pass
+
+def write_post_calls_to_files(name, is_id, url, h_file, m_file):
+    if (is_id):
+        h_file.write('- (void)get'+name+'WithID:(NSNumber*)theID  andSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure{\n')
+        m_file.write('- (void)get'+name+'WithID:(NSNumber*)theID  andSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n')
+    else:
+        h_file.write('- (void)get'+name+'WithSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure{\n')
+        m_file.write('- (void)get'+name+'WithSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n')
+    h_file.write('    [[RKObjectManager sharedManager] getObjectsAtPath:@"'+ url+'" parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {\n')
+    h_file.write('       success(operation,mappingResult);\n')
+    h_file.write('    } failure:^(RKObjectRequestOperation *operation, NSError *error) {\n')
+    h_file.write('       RKLogError(@"Load failed with error: %@", error);\n')
+    h_file.write('       failure(operation,error);\n')
+    h_file.write('    }];\n')
+    h_file.write('}\n\n')
 
 def create_mappings(urls, objects):
 
@@ -38,8 +67,8 @@ def create_mappings(urls, objects):
     with open("MachineDataModel.m", "w") as h, open ("MachineDataModel.h", 'w') as m:
 
         # Setup Mappings
-
         for obj in objects:
+
 
             if "Request" in obj.keys()[0]:
                 requests[obj.keys()[0]] = obj
@@ -47,6 +76,7 @@ def create_mappings(urls, objects):
             params[obj.keys()[0]] = obj[obj.keys()[0]]
 
             if "Parameters" not in obj.keys()[0]:
+
                 name = obj.keys()[0][1].lower() + obj.keys()[0][2:] +'Mapping'
                 obj_name = obj.keys()[0][1].upper() + obj.keys()[0][2:];
                 my_dict[obj_name] = name
@@ -97,7 +127,6 @@ def create_mappings(urls, objects):
                 h.write('\n')
 
                 # Setup Properties
-
                 if len(properties) > 0:
                     for prop in properties:
                         #name = prop.capitalize() + 'Mapping'
@@ -108,75 +137,76 @@ def create_mappings(urls, objects):
 
         m.write('#import "MachineDataModel.h"\n')
         m.write('#import <RestKit/RestKit.h>\n')
-        m.write('#import "AppModel.h"\n')
+        m.write('#import "Viddit-Swift.h"\n')
+
         for request in requests:
             request_variable = request[1].capitalize() + request[2:]
             m.write('#import "'+ request_variable +'.h";\n')
-        # Setup URL Descriptors For Get's
+
+        print "URL's for Patches and Gets"
+        print ''   
         for url in urls:
+            url_api = url['url']
+            get_info = None
+            key_path = 'results'
+            descriptor_name = None
+            mapping = None
             if 'patch' in url:
-                url_api = url['url']
                 get_info = url['patch']
                 key_path = 'nil'
                 if 'keyPath' in url['patch']['response']:
                     key_path = get_info['response']['keyPath']
-                descriptor_name = get_info['response']['200+'][1:] + 'PatchDescriptor'
+                descriptor_name = get_info['request'] + 'RequestPatchDescriptor'
+                mapping = get_info['request'][1:]
+                mapping = mapping[0].upper() + mapping[1:]
+                h.write('RKRequestDescriptor *'+descriptor_name+' = [RKRequestDescriptor requestDescriptorWithMapping:['+my_dict[mapping]+' inverseMapping] objectClass:['+mapping+' class] rootKeyPath:nil method:RKRequestMethodPATCH];\n')
+                h.write('[objectManager addRequestDescriptor:'+descriptor_name+'];\n')
+                h.write('\n')
+            elif 'get' in url and ':id' not in url['url']:
+                meta = url['get']['#meta']
+                get_info = url['get']
+                descriptor_name = get_info['response']['200+'][1:] + 'Descriptor'
                 mapping = get_info['response']['200+'][1:]
-                mapping = mapping[0].upper()+mapping[1:]
-                h.write('RKResponseDescriptor *'+descriptor_name+' = [RKResponseDescriptor responseDescriptorWithMapping:'+my_dict[mapping]+' method:RKRequestMethodPATCH pathPattern:@"'+url_api+'" keyPath:@"'+key_path+'" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];\n')
+                mapping = mapping[0].upper() + mapping[1:]                
+                h.write('RKResponseDescriptor *'+descriptor_name+' = [RKResponseDescriptor responseDescriptorWithMapping:'+my_dict[mapping]+' method:RKRequestMethodGET pathPattern:@"'+url_api+'" keyPath:@"'+key_path+'" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];\n')
                 h.write('[objectManager addResponseDescriptor:'+descriptor_name+'];\n')
                 h.write('\n')
-            if 'get' in url and ':id' not in url['url']:
-                meta = url['get']['#meta']
-                if meta == '' or 'optional' in meta:
-                    url_api = url['url']
-                    get_info = url['get']
+            elif 'post' in url:
+                meta = url['post']['#meta']
+                get_info = url['post']
+                if 'keyPath' in url['post']['response']:
                     key_path = get_info['response']['keyPath']
-                    descriptor_name = get_info['response']['200+'][1:] + 'Descriptor'
-                    mapping = get_info['response']['200+'][1:]
-                    mapping = mapping[0].upper()+mapping[1:]
-                    h.write('RKResponseDescriptor *'+descriptor_name+' = [RKResponseDescriptor responseDescriptorWithMapping:'+my_dict[mapping]+' method:RKRequestMethodGET pathPattern:@"'+url_api+'" keyPath:@"'+key_path+'" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];\n')
-                    h.write('[objectManager addResponseDescriptor:'+descriptor_name+'];\n')
+                    print key_path
+                descriptor_name = get_info['request'][1:] + 'PostDescriptor'
+                mapping = get_info['request'][1:]
+                mapping = mapping[0].upper() + mapping[1:]  
+                h.write('RKRequestDescriptor *'+descriptor_name+' = [RKRequestDescriptor requestDescriptorWithMapping:['+my_dict[mapping]+' inverseMapping] objectClass:['+mapping+' class] rootKeyPath:nil method:RKRequestMethodPOST];\n')
+                h.write('[objectManager addRequestDescriptor:'+descriptor_name+'];\n')
+                h.write('\n')
         h.write('\n')
 
-        print "Posts with no ID's or Parameters"
+        # DATA CALLS
+
+        # GETS
+
+        print "Gets with no ID's, ID's and no Parameters"
         print ''
         for url in urls:
-            if 'get' in url and ':id' not in url['url']:
+            if 'get' in url:
                 meta = url['get']['#meta']
-                if meta == '' or 'optional' in meta:
-                    url = url['url']
-                    h.write('- (void)get'+url[:-1].capitalize()+'WithSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure{\n')
-                    m.write('- (void)get'+url[:-1].capitalize()+'WithSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n')
-                    h.write('    [[RKObjectManager sharedManager] getObjectsAtPath:@"'+ url+'" parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {\n')
-                    h.write('       success(operation,mappingResult);\n')
-                    h.write('    } failure:^(RKObjectRequestOperation *operation, NSError *error) {\n')
-                    h.write('       RKLogError(@"Load failed with error: %@", error);\n')
-                    h.write('       failure(operation,error);\n')
-                    h.write('    }];\n')
-                    h.write('}\n\n')
+                api = url['url']
+                if ':id' not in url['url']:
+                    if meta == '' or 'optional' in meta:
+                         write_post_calls_to_files(api[:-1].capitalize(), False, api, h, m)
+                else:
+                    name = api.split('/')[0]
+                    api = api.replace(":id", "theID")
+                    write_post_calls_to_files(name.capitalize(), True, api, h, m)
         h.write('\n')
-       
-        print "Calls with id's"
-        print ''
-        for url in urls:
-            if 'get' in url and ':id' in url['url']:
-                url = url['url']
-                urls_split = url.split('/')
-                name = urls_split[0]
-                url = url.replace(":id","theID")
-                h.write('- (void)get'+name.capitalize()+'WithID:(NSNumber*)theID  andSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure{\n')
-                m.write('- (void)get'+name.capitalize()+'WithID:(NSNumber*)theID  andSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *result))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n')
-                h.write('    [[RKObjectManager sharedManager] getObjectsAtPath:@"'+ url+'" parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {\n')
-                h.write('       success(operation,mappingResult);\n')
-                h.write('    } failure:^(RKObjectRequestOperation *operation, NSError *error) {\n')
-                h.write('       RKLogError(@"Load failed with error: %@", error);\n')
-                h.write('       failure(operation,error);\n')
-                h.write('    }];\n')
-                h.write('}\n\n')
-        h.write('\n')
+        
+        # GETS (PARAMETERS)
 
-        print "Calls with parameters"
+        print "Gets with parameters"
         print ''
         login = None
         for url in urls:
@@ -224,6 +254,8 @@ def create_mappings(urls, objects):
                     h.write('}\n\n')
 
         for url in urls:
+            
+            # POSTS
             if 'post' in url:
                 post_title = url['url'].replace('/',"").replace(":id",'')
                 post_title = post_title.replace('_',"")
@@ -239,8 +271,8 @@ def create_mappings(urls, objects):
                     method_title += title_variable+":("+variable_type+")"+variable_variable + " "
                 if "id" in url['url']: 
                     method_title += "theID:(NSNumber*) theID "   
-                method_title_h = method_title[:-1] + ";\n"
-                method_title = method_title[:-1] + " {\n"
+                method_title_h = method_title[:-1] + " success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n"
+                method_title = method_title[:-1] + " success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure {\n"
                 h.write(method_title)
                 m.write(method_title_h)
                 h.write('    RKObjectManager* sharedMgr = [RKObjectManager sharedManager];\n')
@@ -248,7 +280,6 @@ def create_mappings(urls, objects):
                 h.write('    ' + request_variable + '* obj = [' + request_variable + ' new];\n')
                 for field in post_request[post_request_key]: 
                     obj_variable = field   
-                    #print "FIELD = " + field    
                     if obj_variable == "id":
                         obj_variable = "theID"
                     elif obj_variable == "description":
@@ -264,14 +295,14 @@ def create_mappings(urls, objects):
                     method_variable = field.replace('_',"")
                     h.write('    obj.'+obj_variable+' = ' + method_variable + ';\n') 
                 url_destination = str(url['url'])
-                # if "id" in url_destination:
-                #     url_destination.replace("s",":theID")
-                h.write('    [sharedMgr.HTTPClient setAuthorizationHeaderWithToken:[AppModel sharedModel].token];\n')
+                h.write('    [sharedMgr.HTTPClient setAuthorizationHeaderWithToken:[AppModel sharedModel].accessToken];\n')
                 h.write('    [sharedMgr postObject:obj path:@"'+url_destination+'" parameters:nil success:^(RKObjectRequestOperation *operation,\n') 
                 h.write('        RKMappingResult *mappingResult) {\n')
                 h.write('        success(operation, mappingResult); }\n') 
                 h.write('        failure:failure];\n')
                 h.write('}\n')
+
+            # PATCH
             elif 'patch' in url:
                 if "id" in url['url']:
                     patch_title = url['url'].replace('/',"")
@@ -288,8 +319,8 @@ def create_mappings(urls, objects):
                         variable_type = OBJC_DATA_TYPES[variable_type[0]]
                         method_title += title_variable+":("+variable_type+")"+variable_variable + " "
                     method_title += "theID:(NSNumber*) theID "   
-                    method_title_h = method_title[:-1] + ";\n"
-                    method_title = method_title[:-1] + " {\n"
+                    method_title_h = method_title[:-1] + " success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n"
+                    method_title = method_title[:-1] + " success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure {\n"
                     h.write(method_title)
                     m.write(method_title_h)
                     h.write('    RKObjectManager* sharedMgr = [RKObjectManager sharedManager];\n')
@@ -297,7 +328,6 @@ def create_mappings(urls, objects):
                     h.write('    ' + request_variable + '* obj = [' + request_variable + ' new];\n')
                     for field in patch_request[patch_request_key]: 
                         obj_variable = field   
-                        #print "FIELD = " + field    
                         if obj_variable == "id":
                             obj_variable = "theID"
                         elif obj_variable == "description":
@@ -312,23 +342,16 @@ def create_mappings(urls, objects):
                         method_variable = field.replace('_',"")
                         h.write('    obj.'+obj_variable+' = ' + method_variable + ';\n') 
                     url_destination = str(url['url'])
-                    # if "id" in url_destination:
-                    #     url_destination.replace("s",":theID")
-                    h.write('    [sharedMgr.HTTPClient setAuthorizationHeaderWithToken:[AppModel sharedModel].token];\n')
+                    h.write('    [sharedMgr.HTTPClient setAuthorizationHeaderWithToken:[AppModel sharedModel].accessToken];\n')
                     h.write('    [sharedMgr patchObject:obj path:@"'+url_destination+'" parameters:nil success:^(RKObjectRequestOperation *operation,\n') 
                     h.write('        RKMappingResult *mappingResult) {\n')
                     h.write('        success(operation, mappingResult); }\n') 
                     h.write('        failure:failure];\n')
                     h.write('}\n')                                                
+        
+        # LOGIN
 
-        h.write('\n')
-        h.write('- (void)getAllLoginWithUsername:(NSString*)username password:(NSString*)password success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure {\n')
-        h.write('   RKObjectManager* sharedMgr = [RKObjectManager sharedManager];\n')
-        h.write('   [sharedMgr.HTTPClient setAuthorizationHeaderWithUsername:username password:password];\n')
-        h.write('   [sharedMgr getObjectsAtPath:@"login/" parameters:nil success:success failure:failure];\n')
-        h.write('}\n')
-
-        m.write('- (void)getAllLoginWithUsername:(NSString*)username password:(NSString*)password success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;\n')
+        write_login_to_files(h, m)
 
 if __name__ == "__main__":
     print 'Please enter your JSON file!'
