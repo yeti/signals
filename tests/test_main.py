7f80346f8aae6ef1411da2cde84e3dc291d2bde0
Rudy Mutter <rudy@yeti.co>
2015-07-09 16:27:43 -0700

    fixed tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ from tests.utils import captured_stderr, captured_stdout
 class MainTestCase(unittest.TestCase):
     def test_run_command(self):
         command = "python -m signals --schema ./tests/files/test_schema.json --generator ios " \
-                  "--datamodels ./tests/files/ --projectname YetiProject"
+                  "--datamodels ./tests/files/ --projectname YetiProject --apiurl http://test.com/api/v1/"
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
         self.assertEqual(error, "")
@@ -15,6 +15,7 @@ class MainTestCase(unittest.TestCase):
 
     def test_run_main(self):
         with captured_stderr() as error, captured_stdout() as out:
-            run_main("./tests/files/test_schema.json", "ios", "./tests/files/", None, "YetiProject")
+            run_main("./tests/files/test_schema.json", "ios", "./tests/files/", None, "YetiProject",
+                     "http://test.com/api/v1/")
             self.assertEqual(error.getvalue(), "")
             self.assertIn("Finished generating your files!", out.getvalue())
