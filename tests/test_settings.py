c0d1cee92f665f82dba2c227873ed4abdf65b7bb
Lee McDole <lee@yeti.co>
2015-07-09 18:19:24 -0700

    testing

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -2,11 +2,10 @@ import mock
 import unittest
 import subprocess
 
-
 class SettingsTestCase(unittest.TestCase):
     @mock.patch("yak_communication.generators.ios.ios_generator.subprocess")
     def test_run_command(self, mock_subprocess):
-        mock_subprocess.check_output.return_value = ""
+        mock_subprocess.check_output.return_value = "Xcode.app"
         command = "python -m yak_communication --schema ./tests/files/test_schema.json --generator ios " \
                   "--datamodels ./tests/files/ --coredata ./tests/files/dummycontents --projectname YetiProject"
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
