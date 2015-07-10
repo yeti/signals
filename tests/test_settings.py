2c3b3326af0964cdc932b75cdd8033987f348969
Lee McDole <lee@yeti.co>
2015-07-10 10:30:14 -0700

    testing

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,15 +3,20 @@ import unittest
 import subprocess
 
 class SettingsTestCase(unittest.TestCase):
-    @mock.patch("yak_communication.generators.ios.ios_generator.subprocess")
+    def empty_string(self):
+        return ""
+
+    # @mock.patch("yak_communication.generators.ios.ios_generator.subprocess")
+    @mock.patch('subprocess.check_output', side_effect=empty_string)
     def test_run_command(self, mock_subprocess):
-        mock_subprocess.check_output.return_value = ""
+        # mock_subprocess.check_output.return_value = ""
         command = "python -m yak_communication --schema ./tests/files/test_schema.json --generator ios " \
                   "--datamodels ./tests/files/ --coredata ./tests/files/dummycontents --projectname YetiProject"
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
         self.assertEqual(error, "")
         self.assertIn("Finished generating your files!", output)
+
 '''
         # Verify successful generation using the --settingspath argument
         command = "python -m yak_communication --settingspath ./tests/files"
