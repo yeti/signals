67cb1299ee6788ab6394dc7ff4f8d0413170acb8
Lee McDole <lee@yeti.co>
2015-09-18 13:45:02 -0700

    updated DataModel to use a delegate for URL and token acquisition

--- tests/generators/ios/files/APIMethod.m
+++ tests/generators/ios/files/APIMethod.m
@@ -1,6 +1,6 @@
 - (void) getPostWithSuccess:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure {
   RKObjectManager* sharedMgr = [RKObjectManager sharedManager];
   sharedMgr.requestSerializationMIMEType = RKMIMETypeJSON;
-  [sharedMgr.HTTPClient setDefaultHeader:@"Authorization" value:[NSString stringWithFormat:@"Bearer %@", [AppModel sharedModel].accessToken]];
+  [sharedMgr.HTTPClient setDefaultHeader:@"Authorization" value:[NSString stringWithFormat:@"Bearer %@", [_delegate getAccessToken]]];
   [sharedMgr getObject:nil path:@"post/" parameters:nil success:success failure:failure];
 }
