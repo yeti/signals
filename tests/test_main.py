82de57eaf673996ca1e1b45f08b56f7428ca7a92
Alex <alex@yetihq.com>
2015-10-13 16:50:04 -0700

    fixing some failing tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,8 +10,8 @@ from tests.utils import captured_stderr, captured_stdout
 
 class MainTestCase(unittest.TestCase):
     def test_run_command(self):
-        command = "python -m signals --schema ./tests/files/test_schema.json --generator ios " \
-                  "--datamodels ./tests/files/ --projectname YetiProject --templates objc"
+        command = "python -m signals --schema ./tests/files/test_schema.json --generator objc " \
+                  "--datamodels ./tests/files/ --projectname YetiProject"
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
         self.assertEqual(error, "")
@@ -19,7 +19,7 @@ class MainTestCase(unittest.TestCase):
 
     def test_run_main(self):
         with captured_stderr() as error, captured_stdout() as out:
-            run_main("./tests/files/test_schema.json", "ios", "objc", "./tests/files/", None, "YetiProject", False)
+            run_main("./tests/files/test_schema.json", "objc", "./tests/files/", None, "YetiProject", False)
             self.assertEqual(error.getvalue(), "")
             self.assertIn("Finished generating your files!", out.getvalue())
 
@@ -27,7 +27,7 @@ class MainTestCase(unittest.TestCase):
     def test_run_main_error(self, mock_subprocess):
         mock_subprocess.check_output.return_value = "Xcode.app"
         with captured_stdout() as out:
-            run_main("./tests/files/test_schema.json", "ios", "objc", "./tests/files/", "./core/data/path",
+            run_main("./tests/files/test_schema.json", "objc", "./tests/files/", "./core/data/path",
                      "YetiProject", False)
             self.assertIn("Must quit Xcode before writing to core data", out.getvalue())
 
