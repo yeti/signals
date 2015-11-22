85ce83bb7d6429f817c5d5ab5adf69568a83cab0
Rudy Mutter <rudy@yeti.co>
2015-11-21 21:33:31 -0800

    fixed entity mapping test for swift

--- tests/generators/ios/swift/files/EntityMapping.swift
+++ tests/generators/ios/swift/files/EntityMapping.swift
@@ -1,3 +1,3 @@
     let postResponseMapping = RKEntityMapping(forEntityForName: "PostResponse", inManagedObjectStore: managedObjectStore)
-    postResponseMapping.identificationAttributes = ["\(theID)"]
+    postResponseMapping.identificationAttributes = ["theID"]
     postResponseMapping.addAttributeMappingsFromDictionary([ "body": "body", "id": "theID", "title": "title" ])
