b7402f1e86c89e18bf83f6148d003787dbe1bc69
brianleung <bleungwh@gmail.com>
2016-01-14 11:13:13 -0800

    Implement Converter classes in conversion.py

--- signals/generators/ios/conversion.py
+++ signals/generators/ios/conversion.py
@@ -1,41 +1,44 @@
 """
 Methods to translate the schema variable names and types to desired language's names and types.
 """
+from signals.logging import warn
+import reserved_mappings
 
-# More listed here: http://www.binpress.com/tutorial/objective-c-reserved-keywords/43
-RESERVED_MAPPINGS = {
-    "auto": "isAuto",
-    "default": "isDefault",
-    "description": "theDescription",
-    "id": "theID",
-    "register": "theRegister",
-    "restrict": "shouldRestrict",
-    "super": "isSuper",
-    "volatile": "isVolatile"
-}
+class BaseConverter(object):
+
+    # Some field names are reserved in target language
+    @classmethod
+    def sanitize_field_name(cls, field_name):
+        if field_name in cls.reserved_mappings:
+            converted_name = cls.reserved_mappings[field_name]
+            warn("{} is a reserved keyword in {} and is now being converted to {}".format(field_name, cls.language, converted_name))
+            return converted_name
+        else:
+            return field_name
+
+    # Changes a Python formatted variable name to conform to target language
+    @classmethod
+    def format_name(cls, python_variable_name, capitalize_first=False):
+        words = python_variable_name.split('_')
+
+        def upper_camel_case(words_to_capitalize):
+            return "".join(word.capitalize() for word in words_to_capitalize)
+
+        if capitalize_first:
+            return upper_camel_case(words)
+        else:
+            return words[0] + upper_camel_case(words[1:])
+
+    @classmethod
+    def get_proper_name(cls, name, capitalize_first=False):
+        proper_name = cls.sanitize_field_name(name)
+        return cls.format_name(proper_name, capitalize_first=capitalize_first)
 
 
-# Changes a Python variable name to an Objective-C/Swift version
-def python_to_objc_variable(python_variable_name, capitalize_first=False):
-    words = python_variable_name.split('_')
+class ObjectiveCConverter(BaseConverter):
+    language = "Objective-C"
+    reserved_mappings = reserved_mappings.OBJC_RESERVED_MAPPINGS
 
-    def upper_camel_case(words_to_capitalize):
-        return "".join(word.capitalize() for word in words_to_capitalize)
-
-    if capitalize_first:
-        return upper_camel_case(words)
-    else:
-        return words[0] + upper_camel_case(words[1:])
-
-
-# Some field names are reserved in Objective C
-def sanitize_field_name(field_name):
-    if field_name in RESERVED_MAPPINGS:
-        return RESERVED_MAPPINGS[field_name]
-    else:
-        return field_name
-
-
-def get_proper_name(name, capitalize_first=False):
-    proper_name = sanitize_field_name(name)
-    return python_to_objc_variable(proper_name, capitalize_first=capitalize_first)
+class SwiftConverter(BaseConverter):
+    language = "Swift"
+    reserved_mappings = reserved_mappings.SWIFT_RESERVED_MAPPINGS
